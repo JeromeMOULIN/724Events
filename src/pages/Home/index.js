a1671b0311ea0d4190765a0ee585072e3f9856bf
JeromeMOULIN <j.moulin89@laposte.net>
2024-02-12 16:46:08 +0100

    Correction carousel oversizing

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -118,11 +118,11 @@ const Page = () => {
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
         <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
+          imageSrc={last?.cover ?? '-'}
+          title={last?.title  ?? '-'}
           date={new Date(last?.date)}
           small
-          label={last?.type}
+          label={last?.type  ?? '-'}
         />
       </div>
       <div className="col contact">
